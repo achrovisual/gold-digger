8cd1561d53343feb9af5e9a6c974d02c9b4c5670
RafMdrzo <rafael_maderazo@dlsu.edu.ph>
2021-11-22 20:39:41 +0800

    added rotate and move, scan and check pending

--- miner.py
+++ miner.py
@@ -8,13 +8,32 @@ class Miner(Object):
         self.scanned = None
     def move(self):
         # Update coordinates here
-        pass
+        if self.compass == 'east': #add a condition that not at the edge of the map
+            self.coordinates['x']+=1
+        elif self.compass=='north' and self.coordinates['y'] != 1:
+            self.coordinates['y']-=1
+        elif self.compass=='south': #add a condition that not at the edge of the map
+            self.coordinates['y']+=1
+        elif self.compass=='west' and self.coordinates['x'] != 1:
+            self.coordinates['x']-=1
+
     def rotate(self):
         # Update compass here
-        pass
+        if self.compass == 'north':
+            self.compass = 'east'
+        elif self.compass == 'east':
+            self.compass = 'south'
+        elif self.compass == 'south':
+            self.compass = 'west'
+        elif self.compass == 'west':
+            self.compass = 'north'
+
     def scan(self):
         # Update scanned here
-        pass
+        returner = None
+
+        #visit front of miner
+        
     def check():
-        # Check if current block is goa
+        # Check if current block is goal
         pass
