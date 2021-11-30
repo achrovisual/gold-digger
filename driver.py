21ed9c0f8b3b7355e58f62bb89e1e329ec5816e0
kerbychuable <kerbychua@gmail.com>
2021-11-30 20:47:04 +0800

    GUI init

--- driver.py
+++ driver.py
@@ -1,7 +1,19 @@
 from grid import Grid
 from bfs import BFS
 from gbfs import gbfs
+import pygame
 
 game = Grid(8)
 
-smart = gbfs(game)
+start = False
+while not start:
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			start = True
+
+smart = BFS(game)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+            pygame.quit()
